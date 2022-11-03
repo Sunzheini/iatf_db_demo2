6e90d5eca121ba7d161382e90a737ad21adf5f98
Sunzheini <daniel_zorov@abv.bg>
2022-11-03 17:13:35 +0200

    update2_03112022

--- iatf_db_demo2/processapp/urls.py
+++ iatf_db_demo2/processapp/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path, include
-from iatf_db_demo2.processapp.views import index, show_page_2
+from iatf_db_demo2.processapp.views import index, processes_description, overview, redirect_to
 
 urlpatterns = (
     # http://127.0.0.1:8000/
     path('', index, name='index'),
-    # http://127.0.0.1:8000/page2/
-    path('page2/', show_page_2, name='show page 2'),
+
+    path('processes-description/', include([
+
+        # http://127.0.0.1:8000/processes-description/
+        path('', overview, name='overview'),
+        # http://127.0.0.1:8000/processes-description/1
+        path('<int:process_id>/', processes_description, name='show specific process'),
+        # http://127.0.0.1:8000/processes-description/redirect/
+        path('redirect/', redirect_to, name='redirect')
+    ])),
 )
--- iatf_db_demo2/processapp/views.py
+++ iatf_db_demo2/processapp/views.py
@@ -28,6 +28,7 @@ def index(request):
     context = {
         'process_steps': all_steps,
         'form': process_form,
+        # from form
         'step_name': step_name,
         'responsible': responsible,
         'evidence': evidence,
@@ -35,8 +36,30 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-def show_page_2(request):
-    # specific_step = Process.objects.get(pk=1)
+def overview(request):
     all_steps = Process.objects.all()
     result = ', '.join([f'{s.step_name} - {s.responsible}' for s in all_steps])
-    return http.HttpResponse(result)
+    context = {
+        'result': result,
+    }
+
+    # return http.HttpResponse(result)
+    return render(request, 'overview.html', context)
+
+
+def processes_description(request, process_id):
+    specific_step = Process.objects.get(pk=1)   # ToDo hardcoded currently
+    context = {
+        'specific_step': specific_step,
+    }
+
+    # return http.HttpResponse(f"{specific_step.step_name} - {specific_step.responsible}")
+    return render(request, 'process_description.html', context)
+
+
+def redirect_to(request):
+    # return redirect('overview')                         # option 1 name of the url in urls.py
+    # return redirect('https://www.abv.bg/')
+    # return redirect('/processes-description/1/')        # option 2 hardcoded url path
+
+    return redirect('show specific process', process_id=1)     # option 3 for cases like .../pk
